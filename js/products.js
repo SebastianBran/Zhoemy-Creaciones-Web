b6f44dce3b7dabb651996e262058cd3a82a2c0a9
SebastianBran <sebastianbranzapata@gmail.com>
2021-02-02 00:09:45 -0500

    added modal page for the products and restructured all the project

--- js/products.js
+++ js/products.js
@@ -4,7 +4,7 @@ class Product {
         this.id_image = id_image;
         this.url_image = url_image;
     }
-
+    
     load_Ui() {
         let index = `
             <div class="item-gallery">
@@ -37,6 +37,37 @@ let products = [
     new Product("Prenda 12", "p12", "/assets/images/products/12.png")
 ];
 
+/* load UI of product */
 for(let i = 0; i < products.length; i++) {
     products[i].load_Ui();
 }
+
+/* expand the image selected */
+let elements = document.querySelectorAll(".img-container");
+
+elements.forEach(element => {
+    let url = element.style.backgroundImage.toString();
+
+    element.addEventListener("click", () => {
+        let image_expand_container = document.getElementById("image-expand-container");
+
+        document.querySelector("body").style.overflow = "hidden"; /* block the scroll in the page */
+
+        document.getElementById("image-expand").classList.toggle("active");
+        document.getElementById("image-expand-bg").classList.toggle("active");
+        document.getElementById("image-expand-close").classList.toggle("active");
+        image_expand_container.classList.toggle("active");
+
+        image_expand_container.style.backgroundImage = `url(${url.slice(5, url.length - 2)})`;
+    });
+});
+
+/* close the expand image */
+document.querySelector(".fas").addEventListener("click", () => {
+    document.querySelector("body").style.overflow = "visible";
+
+    document.getElementById("image-expand").classList.toggle("active");
+    document.getElementById("image-expand-bg").classList.toggle("active");
+    document.getElementById("image-expand-close").classList.toggle("active");
+    document.getElementById("image-expand-container").classList.toggle("active");
+});
